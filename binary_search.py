f1776d8c55d47275715051866533edb43f194ddb
Ganga R <ganga11222005@gmail.com>
2026-03-17 10:33:17 +0530

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,32 @@
+# binary search
+
+def binary_search(array, key):
+    low = 0
+    high = len(array) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if array[mid] == key:
+            return mid
+        elif array[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+# Ueser input
+n = int(input("Enter the number of elements:"))
+print("*** Enter the element in sorted order ***")
+array = []
+
+for i in range(n):
+    array.append(int(input()))
+
+key = int(input("Enter the key to be searched:"))
+result = binary_search(array, key)
+
+if result != -1:
+    print("Element found at the :",result)
+else:
+    print("Element not found")
